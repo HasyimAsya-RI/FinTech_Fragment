ef6de68849e65557b08e066e531659eb3f335000
Muhammad Hasyim Asy'ari <hasy1m@MacBook-Air-Muhammad.local>
2022-04-25 21:29:18 +0700

    3rd Practicum

---
+++ app/src/main/java/com/example/fintech_3fragment/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.fintech_3fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.fintech_3fragment.home.HomeFragment
+import com.example.fintech_3fragment.profile.ProfileFragment
+import com.example.fintech_3fragment.setting.SettingFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnHome: Button
+    private lateinit var btnProfile: Button
+    private lateinit var btnSetting: Button
+
+    override fun onCreate( savedInstanceState: Bundle? ) {
+        super.onCreate( savedInstanceState )
+        setContentView( R.layout.activity_main )
+
+        btnHome     = findViewById( R.id.btn_home )
+        btnProfile  = findViewById( R.id.btn_profile )
+        btnSetting  = findViewById( R.id.btn_setting )
+
+//      btnHome.setOnClickListener {
+//          val intent = Intent( this, HomeActivity::class.java )
+//          intent.putExtra( "username", "hasyim" )
+//          startActivity( intent )
+//      }
+
+        btnHome.setOnClickListener {
+            loadFragment( HomeFragment() )
+        }
+        btnProfile.setOnClickListener {
+            loadFragment( ProfileFragment() )
+        }
+        btnSetting.setOnClickListener {
+            loadFragment( SettingFragment() )
+        }
+    }
+
+    private fun loadFragment( fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace( R.id.container, fragment )
+            .commitNow()
+    }
+}
